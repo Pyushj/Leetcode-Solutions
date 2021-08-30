1197dc0532a83145b833d47898877d49639bb04a
Pyushj <87221338+Pyushj@users.noreply.github.com>
2021-08-30 22:01:35 +0530

    Create 515. Find Largest Value in Each Tree Row.cpp

---
+++ 515. Find Largest Value in Each Tree Row.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int>res;
+        if(!root)
+            return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(q.size()>0)
+        {
+            int size=q.size(),mx=INT_MIN;
+            while(size--)
+            {
+                TreeNode *temp=q.front();
+                q.pop();
+                mx=max(mx,temp->val);
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+            }
+            res.push_back(mx);
+        }
+        return res;
+    }
+};

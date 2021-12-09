5e119bfd36ef80ef1ebe484025d78d91617d60d2
Pyushj <87221338+Pyushj@users.noreply.github.com>
2021-12-09 17:12:24 +0530

    Create 538. Convert BST to Greater Tree.cpp
    Perform inorder using rignt node left technique and update data into the same tree

---
+++ 538. Convert BST to Greater Tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root,int& sum)
+    {
+        if(!root)
+            return;
+        inorder(root->right,sum);
+        root->val+=sum;
+        sum=root->val;
+        inorder(root->left,sum);
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        int sum=0;
+        inorder(root,sum);
+        
+        return root;
+        
+    }
+};
